dd5e124e092fe14bb96f6f4e889af54eca78c50e
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-24 08:54:40 -0300

    add JwtUtil class for JWT token generation and validation

---
+++ src/main/java/com/lucianoribeiro/helpdesk/config/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.lucianoribeiro.helpdesk.config;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@Getter
+public class JwtUtil {
+
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long expirationMillis = 1000 * 60 * 60 * 5;
+
+    public static String generateToken(String subject) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMillis))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+}
+
