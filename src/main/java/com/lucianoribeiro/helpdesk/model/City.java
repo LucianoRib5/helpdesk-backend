88ab2eb6636fdbe8032386c2ab4b30dcca1dc2cb
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-24 14:22:19 -0300

    add City entity with fields for id, name, and state relationship

---
+++ src/main/java/com/lucianoribeiro/helpdesk/model/City.java
@@ -0,0 +1,29 @@
+package com.lucianoribeiro.helpdesk.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "cities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "state_id")
+    private State state;
+
+}
