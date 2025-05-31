c2e1bfad1af7ca1096e18fdc15745d930fedd787
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-31 13:49:24 -0300

    add CustomerController, CustomerService, and CustomerDTO for managing customer data

--- src/main/java/com/lucianoribeiro/helpdesk/repository/CustomerRepository.java
+++ src/main/java/com/lucianoribeiro/helpdesk/repository/CustomerRepository.java
@@ -4,4 +4,6 @@ import com.lucianoribeiro.helpdesk.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByUserId(Long userId);
 }
